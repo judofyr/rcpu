855f435d6c653a728c50575b112ef0677547c6bf
Magnus Holm <judofyr@gmail.com>
2012-04-05 00:12:00 +0200

    Refactor test case

--- test/test_emulator.rb
+++ test/test_emulator.rb
@@ -21,15 +21,11 @@ module RCPU
       @emu.registers[name]
     end
 
-    def test_set
+    def block(&blk)
       rcpu do
         program :main do
           block :init do
-            SET a, 0x30
-            SET [0x2000], a
-
-            SET b, 0x2001
-            SET [b], 0x10
+            instance_eval(&blk)
             jump :crash
           end
 
@@ -37,6 +33,17 @@ module RCPU
             jump :crash
           end
         end
+      end
+    end
+
+    def test_set
+      block do
+        SET a, 0x30
+        SET [0x2000], a
+
+        SET b, 0x2001
+        SET [b], 0x10
+        jump :crash
       end
 
       assert_equal 0x30, memory[0x2000]
@@ -44,22 +51,14 @@ module RCPU
     end
 
     def test_add
-      rcpu do
-        program :main do
-          block :init do
-            SET [0x1000], 0x5678    # low word
-            SET [0x1001], 0x1234    # high word
-            ADD [0x1000], 0xCCDD    # add low words, sets O to either 0 or 1 (in this case 1)
-            ADD [0x1001], o         # add O to the high word
-            ADD [0x1001], 0xAABB
-            ADD [0x1001], o
-            jump :crash
-          end
-
-          block :crash do
-            jump :crash
-          end
-        end
+      block do
+        SET [0x1000], 0x5678    # low word
+        SET [0x1001], 0x1234    # high word
+        ADD [0x1000], 0xCCDD    # add low words, sets O to either 0 or 1 (in this case 1)
+        ADD [0x1001], o         # add O to the high word
+        ADD [0x1001], 0xAABB
+        ADD [0x1001], o
+        jump :crash
       end
 
       assert_equal 0x2355, memory[0x1000]
@@ -67,18 +66,10 @@ module RCPU
     end
 
     def test_sub
-      rcpu do
-        program :main do
-          block :init do
-            SET [0x1000], 0x5678
-            SUB [0x1000], 0xCCDD
-            jump :crash
-          end
-
-          block :crash do
-            jump :crash
-          end
-        end
+      block  do
+        SET [0x1000], 0x5678
+        SUB [0x1000], 0xCCDD
+        jump :crash
       end
 
       assert_equal 0x899B, memory[0x1000]
